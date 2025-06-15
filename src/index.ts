26fd9d1b187bd3d6b99abc5cfa27af539b7a9bc8
weili.dai <weili.dai@gumtree.com>
2025-06-15 11:44:59 +0100

    Added scaffolding

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { add } from './math';
+
+// Basic TypeScript example
+const greeting: string = 'Hello, TypeScript with ts-node!';
+console.log(greeting);
+
+// Using the imported add function
+console.log(`2 + 3 = ${add(2, 3)}`);
---
+++ src/math.ts
@@ -0,0 +1,5 @@
+export const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+// You can add more math utility functions here in the future
